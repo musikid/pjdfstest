5a206f140293d80c0d8e769f3a6079a2f5e8d13a
musikid <musikid@outlook.com>
2022-06-07 10:16:55 +0200

    feat: add first test

--- rust/src/tests/chmod/mod.rs
+++ rust/src/tests/chmod/mod.rs
@@ -1,4 +1,13 @@
-mod permission;
 mod lchmod;
+mod permission;
+
+fn chmod<P: ?Sized + nix::NixPath>(path: &P, mode: nix::sys::stat::Mode) -> nix::Result<()> {
+    nix::sys::stat::fchmodat(
+        None,
+        path,
+        mode,
+        nix::sys::stat::FchmodatFlags::FollowSymlink,
+    )
+}
 
 crate::pjdfs_group!(chmod; permission::test_case);
--- rust/src/tests/chmod/permission.rs
+++ rust/src/tests/chmod/permission.rs
@@ -1,11 +1,31 @@
+use std::{thread::sleep, time::Duration};
+
 use crate::{
     pjdfs_test_case,
-    test::{TestContext, TestResult},
+    runner::context::FileType,
+    test::{TestContext, TestError, TestResult},
+    test_assert, test_assert_eq,
+    tests::chmod::chmod,
 };
+use nix::sys::stat::{stat, Mode};
+use strum::IntoEnumIterator;
 
 // chmod/00.t:L58
 fn test_ctime(ctx: &mut TestContext) -> TestResult {
-    println!("testing!");
+    for f_type in FileType::iter() {
+        if f_type == FileType::Symlink {
+            continue;
+        }
+        let path = ctx.create(f_type).map_err(TestError::CreateFile)?;
+        let ctime_before = stat(&path)?.st_ctime;
+
+        sleep(Duration::from_secs(1));
+
+        chmod(&path, Mode::from_bits_truncate(0o111))?;
+
+        let ctime_after = stat(&path)?.st_ctime;
+        test_assert!(ctime_after > ctime_before);
+    }
 
     Ok(())
 }
