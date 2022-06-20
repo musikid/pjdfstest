f257903f3cf5e9148785383098c5cfbce6bda55f
musikid <musikid@outlook.com>
2022-06-20 21:30:01 +0200

    feat: improve runner

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{stdout, Write};
+
 use pjdfs_tests::{pjdfs_main, test::TestContext, tests::chmod};
 
-pjdfs_main!(chmod::tests);
+fn main() -> anyhow::Result<()> {
+    for group in [chmod::tests] {
+        for test_case in group.test_cases.iter() {
+            for test in test_case.tests {
+                print!(
+                    "{}\t",
+                    format!("{}::{}::{}", group.name, test_case.name, test.name)
+                );
+                stdout().lock().flush()?;
+                let mut context = TestContext::new();
+                match (test.fun)(&mut context) {
+                    Ok(_) => println!("success"),
+                    Err(e) => return Err(anyhow::anyhow!("{}", e)),
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
