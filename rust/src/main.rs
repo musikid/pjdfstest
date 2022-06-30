d35edd0025e55f6aa3ecb93b89974339d68bbabe
Sayafdine Said <musikid@outlook.com>
2022-06-30 18:09:54 +0200

    feat: replace result with unwrap (#27)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,28 @@
-use std::io::{stdout, Write};
+use std::{
+    io::{stdout, Write},
+    panic::{catch_unwind, set_hook, AssertUnwindSafe, Location, PanicInfo},
+};
 
+use once_cell::sync::OnceCell;
 use pjdfs_tests::{pjdfs_main, test::TestContext, tests::chmod};
 
+struct PanicLocation(u32, u32, String);
+
+static PANIC_LOCATION: OnceCell<PanicLocation> = OnceCell::new();
+
 fn main() -> anyhow::Result<()> {
+    set_hook(Box::new(|ctx| {
+        if let Some(location) = ctx.location() {
+            let _ = PANIC_LOCATION.set(PanicLocation(
+                location.line(),
+                location.column(),
+                location.file().into(),
+            ));
+        } else {
+            unimplemented!()
+        }
+    }));
+
     for group in [chmod::tests] {
         for test_case in group.test_cases.iter() {
             for test in test_case.tests {
@@ -12,9 +32,27 @@ fn main() -> anyhow::Result<()> {
                 );
                 stdout().lock().flush()?;
                 let mut context = TestContext::new();
-                match (test.fun)(&mut context) {
+                //TODO: AssertUnwindSafe should be used with caution
+                let mut ctx_wrapper = AssertUnwindSafe(&mut context);
+                match catch_unwind(move || {
+                    (test.fun)(&mut ctx_wrapper);
+                }) {
                     Ok(_) => println!("success"),
-                    Err(e) => return Err(anyhow::anyhow!("{}", e)),
+                    Err(e) => {
+                        let location = PANIC_LOCATION.get().unwrap();
+                        anyhow::bail!(
+                            "{}
+                            Located in file {} at {}:{}
+                            ",
+                            e.downcast_ref::<String>()
+                                .cloned()
+                                .or_else(|| e.downcast_ref::<&str>().map(|&s| s.to_string()))
+                                .unwrap_or_default(),
+                            location.2,
+                            location.0,
+                            location.1
+                        )
+                    }
                 }
             }
         }
--- rust/src/tests/chmod/errno.rs
+++ rust/src/tests/chmod/errno.rs
@@ -3,19 +3,14 @@ use nix::{
     sys::stat::{stat, Mode},
 };
 
-use crate::{
-    pjdfs_test_case,
-    runner::context::FileType,
-    test::{TestContext, TestResult},
-    test_assert, test_assert_eq,
-};
+use crate::{pjdfs_test_case, runner::context::FileType, test::TestContext};
 
 use super::chmod;
 
 pjdfs_test_case!(errno, { test: test_enotdir, require_root: true }, { test: test_enametoolong });
 
 /// Returns ENOTDIR if a component of the path prefix is not a directory
-fn test_enotdir(ctx: &mut TestContext) -> TestResult {
+fn test_enotdir(ctx: &mut TestContext) {
     for f_type in [
         FileType::Regular,
         FileType::Fifo,
@@ -23,29 +18,25 @@ fn test_enotdir(ctx: &mut TestContext) -> TestResult {
         FileType::Char,
         FileType::Socket,
     ] {
-        let not_dir = ctx.create(f_type)?;
+        let not_dir = ctx.create(f_type).unwrap();
         let fake_path = not_dir.join("test");
         let res = chmod(&fake_path, Mode::from_bits_truncate(0o0644));
-        test_assert!(res.is_err());
-        test_assert_eq!(res.unwrap_err(), Errno::ENOTDIR);
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err(), Errno::ENOTDIR);
     }
-
-    Ok(())
 }
 
 /// chmod returns ENAMETOOLONG if a component of a pathname exceeded {NAME_MAX} characters
-fn test_enametoolong(ctx: &mut TestContext) -> TestResult {
-    let path = ctx.create_max(FileType::Regular)?;
+fn test_enametoolong(ctx: &mut TestContext) {
+    let path = ctx.create_max(FileType::Regular).unwrap();
     let expected_mode = 0o620;
-    chmod(&path, Mode::from_bits_truncate(expected_mode))?;
-    let actual_mode = stat(&path)?.st_mode;
-    test_assert_eq!(actual_mode & 0o777, expected_mode);
+    chmod(&path, Mode::from_bits_truncate(expected_mode)).unwrap();
+    let actual_mode = stat(&path).unwrap().st_mode;
+    assert_eq!(actual_mode & 0o777, expected_mode);
 
     let mut too_long_path = path.clone();
     too_long_path.set_extension("x");
     let res = chmod(&too_long_path, Mode::from_bits_truncate(0o0620));
-    test_assert!(res.is_err());
-    test_assert_eq!(res.unwrap_err(), Errno::ENAMETOOLONG);
-
-    Ok(())
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err(), Errno::ENAMETOOLONG);
 }
