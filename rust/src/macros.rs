150eb1a44521e4d37a8dcd2945a9fd8f0745efa0
musikid <musikid@outlook.com>
2022-07-07 11:25:51 +0200

    style: improve formatting

--- rust/src/macros.rs
+++ rust/src/macros.rs
@@ -23,15 +23,15 @@ macro_rules! test_case {
             };
         }
     };
-    ( $f:ident, $syscall:expr, $require_root:expr => [$(FileType::$file_type: tt $(($ft_args: tt))?),+ $(,)*]) => {
+    ( $f:ident, $syscall:expr, $require_root:expr => [$( FileType:: $file_type:tt $( ($ft_args: tt) )? ),+ $(,)*]) => {
         $(
             paste::paste! {
                 #[linkme::distributed_slice($crate::test::TEST_CASES)]
                 static [<CASE_$f:upper$file_type:upper>]: $crate::test::TestCase = crate::test::TestCase {
                     name: concat!(module_path!(), "::", stringify!($f), "::", stringify!([<$file_type:lower>]), "_type"),
                     syscall: $syscall,
-                    require_root: $require_root || FileType::$file_type $( ($ft_args ) )?.privileged(),
-                    fun: |ctx| $f(ctx, FileType::$file_type $( ($ft_args ) )?),
+                    require_root: $require_root || FileType::$file_type $( ($ft_args) )?.privileged(),
+                    fun: |ctx| $f(ctx, FileType::$file_type $( ($ft_args) )?),
                 };
             }
         )+
