90c0b7159ba32744d173094f4d46adda66b7099d
Sayafdine Said <musikid@outlook.com>
2022-08-10 11:18:53 +0200

    feat: add rmdir tests (#93)

---
+++ rust/src/tests/rmdir.rs
@@ -0,0 +1,31 @@
+use std::fs::metadata;
+
+use nix::{errno::Errno, sys::stat::lstat};
+
+use crate::{runner::context::TestContext, tests::assert_mtime_changed, utils::rmdir};
+
+use super::assert_ctime_changed;
+
+crate::test_case! {
+    /// rmdir remove directory
+    remove_dir
+}
+fn remove_dir(ctx: &mut TestContext) {
+    let dir = ctx.create(crate::runner::context::FileType::Dir).unwrap();
+    assert!(metadata(&dir).unwrap().is_dir());
+    assert!(rmdir(&dir).is_ok());
+    assert_eq!(lstat(&dir).unwrap_err(), Errno::ENOENT);
+}
+
+crate::test_case! {
+    /// rmdir updates parent ctime and mtime on success
+    changed_time_parent_success
+}
+fn changed_time_parent_success(ctx: &mut TestContext) {
+    let dir = ctx.create(crate::runner::context::FileType::Dir).unwrap();
+    assert_ctime_changed(ctx, ctx.base_path(), || {
+        assert_mtime_changed(ctx, ctx.base_path(), || {
+            assert!(rmdir(&dir).is_ok());
+        });
+    });
+}
--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -18,6 +18,11 @@ pub fn lchmod<P: ?Sized + nix::NixPath>(path: &P, mode: nix::sys::stat::Mode) ->
     )
 }
 
+pub fn rmdir<P: ?Sized + nix::NixPath>(path: &P) -> nix::Result<()> {
+    let res = path.with_nix_path(|cstr| unsafe { nix::libc::rmdir(cstr.as_ptr()) })?;
+    nix::errno::Errno::result(res).map(std::mem::drop)
+}
+
 pub const ALLPERMS: nix::sys::stat::mode_t = 0o777;
 
 /// Wrapper for `renameat(None, path, None, path)`.
