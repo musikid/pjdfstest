a169711e2f555c027ba524c6cd80e9a8a5a564e7
Sayafdine Said <musikid@outlook.com>
2022-08-26 13:21:47 +0200

    refactor(errors): remove imports

--- rust/src/tests/errors.rs
+++ rust/src/tests/errors.rs
@@ -1,28 +1,3 @@
-use nix::{
-    errno::Errno,
-    fcntl::{open, OFlag},
-    libc::off_t,
-    sys::stat::{lstat, stat, Mode},
-    unistd::{chown, ftruncate, truncate},
-};
-
-#[cfg(any(
-    target_os = "openbsd",
-    target_os = "netbsd",
-    target_os = "freebsd",
-    target_os = "dragonfly",
-    target_os = "macos",
-    target_os = "ios"
-))]
-use nix::{sys::stat::FileFlag, unistd::chflags};
-
-use std::{fmt::Debug, fs::metadata, os::unix::prelude::PermissionsExt, path::Path};
-
-use crate::{
-    runner::context::{FileType, SerializedTestContext, TestContext},
-    utils::{chmod, rename, rmdir, symlink, ALLPERMS, ALLPERMS_STICKY},
-};
-
 mod eacces;
 mod eexist;
 mod efault;
