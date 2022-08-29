412d93bd38928ff6b83fe3cc29f225a0ea57ef2c
Sayafdine Said <musikid@outlook.com>
2022-08-29 16:33:45 +0200

    feat(chmod): add suid/sgid verify

--- rust/src/tests/chmod.rs
+++ rust/src/tests/chmod.rs
@@ -1,3 +1,5 @@
+use std::{fs::metadata, os::unix::prelude::PermissionsExt};
+
 use crate::{
     runner::context::{FileType, SerializedTestContext},
     test::TestContext,
@@ -5,6 +7,7 @@ use crate::{
     utils::{chmod, ALLPERMS},
 };
 use nix::{
+    libc::mode_t,
     sys::stat::{lstat, stat, Mode},
     unistd::chown,
 };
@@ -100,4 +103,35 @@ fn clear_isgid_bit(ctx: &mut SerializedTestContext) {
     let actual_mode = stat(&path).unwrap().st_mode;
     assert_eq!(actual_mode & 0o7777, expected_mode.bits());
     //TODO: FreeBSD "S_ISGID should be removed and chmod(2) should success and FreeBSD returns EPERM."
+}
+
+crate::test_case! {
+    /// verify SUID/SGID bit behaviour
+    verify_suid_sgid, serialized, root
+}
+fn verify_suid_sgid(ctx: &mut SerializedTestContext) {
+    const ALLPERMS_SUID_SGID: mode_t = ALLPERMS | Mode::S_ISUID.bits() | Mode::S_ISGID.bits();
+
+    fn check_suid_sgid(ctx: &mut SerializedTestContext, bits: Mode) {
+        let mode_bits = Mode::from_bits_truncate(0o777) | bits;
+        let mode_without_bits = mode_bits & !bits;
+        let user = ctx.get_new_user();
+        ctx.as_user(&user, None, || {
+            let file = ctx
+                .new_file(FileType::Regular)
+                .mode(mode_bits.bits())
+                .create()
+                .unwrap();
+            std::fs::write(&file, &[]).unwrap();
+            let file_stat = metadata(&file).unwrap();
+            assert_eq!(
+                file_stat.permissions().mode() as mode_t & ALLPERMS_SUID_SGID,
+                mode_without_bits.bits()
+            );
+        });
+    }
+
+    check_suid_sgid(ctx, Mode::S_ISUID);
+    check_suid_sgid(ctx, Mode::S_ISGID);
+    check_suid_sgid(ctx, Mode::S_ISUID | Mode::S_ISGID);
 }
