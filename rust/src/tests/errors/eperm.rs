21dc1f138e258719b7cd69a0741b25616c700b96
Sayafdine Said <musikid@outlook.com>
2022-09-01 11:12:21 +0200

    feat(eperm): add chmod test

--- rust/src/tests/errors/eperm.rs
+++ rust/src/tests/errors/eperm.rs
@@ -1,12 +1,19 @@
+use std::{fs::metadata, os::unix::prelude::MetadataExt};
+
 use crate::{
     runner::context::{FileType, SerializedTestContext},
-    utils::{lchown, link},
+    utils::{chmod, lchown, link, ALLPERMS},
 };
 
 #[cfg(not(target_os = "linux"))]
 use {crate::runner::context::TestContext, nix::unistd::unlink};
 
-use nix::{errno::Errno, unistd::chown};
+use nix::{
+    errno::Errno,
+    libc::mode_t,
+    sys::stat::Mode,
+    unistd::{chown, Uid, User},
+};
 
 #[cfg(any(
     target_os = "openbsd",
@@ -171,4 +178,95 @@ fn chown_euid_not_root_not_owner_symlink(ctx: &mut SerializedTestContext) {
             Err(Errno::EPERM)
         );
     });
+}
+
+crate::test_case! {
+    /// chmod returns EPERM if the operation would change the ownership, but the effective user ID is not the super-user
+    chmod_not_owner, serialized, root
+}
+fn chmod_not_owner(ctx: &mut SerializedTestContext) {
+    let user = ctx.get_new_user();
+    chown(ctx.base_path(), Some(user.uid), Some(user.gid)).unwrap();
+
+    let file = ctx.create(FileType::Regular).unwrap();
+    chown(&file, Some(user.uid), Some(user.gid)).unwrap();
+
+    let mode = Mode::from_bits_truncate(0o642);
+    let new_mode = Mode::from_bits_truncate(0o641);
+
+    ctx.as_user(&user, None, || {
+        assert!(chmod(&file, mode).is_ok());
+        let file_stat = metadata(&file).unwrap();
+        assert_eq!(file_stat.mode() as mode_t & ALLPERMS, mode.bits());
+    });
+
+    let other_user = ctx.get_new_user();
+    ctx.as_user(&other_user, None, || {
+        assert_eq!(chmod(&file, new_mode), Err(Errno::EPERM));
+        let file_stat = metadata(&file).unwrap();
+        assert_eq!(file_stat.mode() as mode_t & ALLPERMS, mode.bits());
+    });
+
+    let current = User::from_uid(Uid::effective()).unwrap().unwrap();
+    chown(&file, Some(current.uid), Some(current.gid)).unwrap();
+
+    ctx.as_user(&user, None, || {
+        assert_eq!(chmod(&file, new_mode), Err(Errno::EPERM));
+        let file_stat = metadata(&file).unwrap();
+        assert_eq!(file_stat.mode() as mode_t & ALLPERMS, mode.bits());
+    });
+
+    // symlink
+    let link = ctx.create(FileType::Symlink(Some(file.clone()))).unwrap();
+    chown(&link, Some(user.uid), Some(user.gid)).unwrap();
+    chown(&file, Some(user.uid), Some(user.gid)).unwrap();
+
+    ctx.as_user(&user, None, || {
+        assert!(chmod(&link, mode).is_ok());
+        let link_stat = metadata(&link).unwrap();
+        assert_eq!(link_stat.mode() as mode_t & ALLPERMS, mode.bits());
+    });
+
+    let other_user = ctx.get_new_user();
+    ctx.as_user(&other_user, None, || {
+        assert_eq!(chmod(&link, new_mode), Err(Errno::EPERM));
+        let link_stat = metadata(&link).unwrap();
+        assert_eq!(link_stat.mode() as mode_t & ALLPERMS, mode.bits());
+    });
+
+    chown(&link, Some(current.uid), Some(current.gid)).unwrap();
+
+    ctx.as_user(&user, None, || {
+        assert_eq!(chmod(&link, new_mode), Err(Errno::EPERM));
+        let link_stat = metadata(&link).unwrap();
+        assert_eq!(link_stat.mode() as mode_t & ALLPERMS, mode.bits());
+    });
+
+    #[cfg(any(target_os = "freebsd", target_os = "netbsd", target_os = "dragonfly"))]
+    {
+        use crate::utils::lchmod;
+
+        chown(&file, Some(user.uid), Some(user.gid)).unwrap();
+
+        ctx.as_user(&user, None, || {
+            assert!(lchmod(&file, mode).is_ok());
+            let file_stat = metadata(&file).unwrap();
+            assert_eq!(file_stat.mode() as mode_t & ALLPERMS, mode.bits());
+        });
+
+        let other_user = ctx.get_new_user();
+        ctx.as_user(&other_user, None, || {
+            assert_eq!(lchmod(&file, new_mode), Err(Errno::EPERM));
+            let file_stat = metadata(&file).unwrap();
+            assert_eq!(file_stat.mode() as mode_t & ALLPERMS, mode.bits());
+        });
+
+        chown(&file, Some(current.uid), Some(current.gid)).unwrap();
+
+        ctx.as_user(&user, None, || {
+            assert_eq!(lchmod(&file, new_mode), Err(Errno::EPERM));
+            let file_stat = metadata(&file).unwrap();
+            assert_eq!(file_stat.mode() as mode_t & ALLPERMS, mode.bits());
+        });
+    }
 }
