6483aa231224bfa39eba119c1aa7bc19277d56ca
Sayafdine Said <musikid@outlook.com>
2022-10-06 19:01:41 +0200

    feat(tests): add emlink tests

--- rust/src/tests/open.rs
+++ rust/src/tests/open.rs
@@ -118,3 +118,40 @@ fn interact_2gb(ctx: &mut TestContext) {
     nix::sys::uio::pread(fd, &mut buf, offset).unwrap();
     assert_eq!(buf, DATA.as_bytes());
 }
+
+// POSIX states that open should return ELOOP, but FreeBSD returns EMLINK instead
+#[cfg(target_os = "freebsd")]
+crate::test_case! {
+    /// open returns EMLINK when O_NOFOLLOW was specified and the target is a symbolic link
+    open_nofollow
+}
+#[cfg(target_os = "freebsd")]
+fn open_nofollow(ctx: &mut TestContext) {
+    use nix::{
+        fcntl::{open, OFlag},
+        sys::stat::Mode,
+    };
+
+    let link = ctx.create(FileType::Symlink(None)).unwrap();
+
+    assert_eq!(
+        open(
+            &link,
+            OFlag::O_RDONLY | OFlag::O_CREAT | OFlag::O_NOFOLLOW,
+            Mode::empty()
+        ),
+        Err(Errno::EMLINK)
+    );
+    assert_eq!(
+        open(&link, OFlag::O_RDONLY | OFlag::O_NOFOLLOW, Mode::empty()),
+        Err(Errno::EMLINK)
+    );
+    assert_eq!(
+        open(&link, OFlag::O_WRONLY | OFlag::O_NOFOLLOW, Mode::empty()),
+        Err(Errno::EMLINK)
+    );
+    assert_eq!(
+        open(&link, OFlag::O_RDWR | OFlag::O_NOFOLLOW, Mode::empty()),
+        Err(Errno::EMLINK)
+    );
+}
