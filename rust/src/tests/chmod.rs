4331290cf88580a76bff0acf6e6d9bf2190aa062
Sayafdine Said <musikid@outlook.com>
2022-10-08 22:48:38 +0200

    feat(chmod): add eftype tests (#118)

--- rust/src/tests/chmod.rs
+++ rust/src/tests/chmod.rs
@@ -9,6 +9,7 @@ use crate::{
 use crate::utils::lchmod;
 
 use nix::{
+    errno::Errno,
     libc::mode_t,
     sys::stat::{lstat, stat, Mode},
     unistd::chown,
@@ -141,3 +142,51 @@ enoent_comp_test_case!(
 );
 
 enoent_symlink_named_file_test_case!(chmod(~path, Mode::empty()));
+
+#[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+crate::test_case! {
+    /// chmod returns EFTYPE if the effective user ID is not the super-user,
+    /// the mode includes the sticky bit (S_ISVTX),
+    /// and path does not refer to a directory
+    // chmod/12.t
+    eftype, serialized, root => [Regular, Fifo, Block, Char, Socket]
+}
+#[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+fn eftype(ctx: &mut SerializedTestContext, ft: FileType) {
+    let user = ctx.get_new_user();
+
+    let original_mode = Mode::from_bits_truncate(0o640);
+    let file = ctx
+        .new_file(ft)
+        .mode(original_mode.bits())
+        .create()
+        .unwrap();
+    chown(&file, Some(user.uid), Some(user.gid)).unwrap();
+    let new_mode = Mode::from_bits_truncate(0o644);
+    let link = ctx.create(FileType::Symlink(Some(file.clone()))).unwrap();
+
+    ctx.as_user(&user, None, || {
+        assert_eq!(chmod(&file, new_mode | Mode::S_ISVTX), Err(Errno::EFTYPE));
+    });
+    let file_stat = stat(&file).unwrap();
+    assert_eq!(file_stat.st_mode & ALLPERMS_STICKY, original_mode.bits());
+
+    ctx.as_user(&user, None, || {
+        assert_eq!(
+            chmod(&link, original_mode | Mode::S_ISVTX),
+            Err(Errno::EFTYPE)
+        );
+    });
+    let file_stat = stat(&link).unwrap();
+    assert_eq!(file_stat.st_mode & ALLPERMS_STICKY, original_mode.bits());
+
+    // lchmod
+
+    let mode = Mode::from_bits_truncate(0o621) | Mode::S_ISVTX;
+    ctx.as_user(&user, None, || {
+        assert_eq!(lchmod(&file, mode), Err(Errno::EFTYPE));
+    });
+
+    let file_stat = lstat(&file).unwrap();
+    assert_eq!(file_stat.st_mode & ALLPERMS_STICKY, original_mode.bits());
+}
